3f2a31adfa9359b13a5a027629a904cb7aeceb45
Maxence Moutoussamy <maxence.moutoussamy@epitech.eu>
2019-03-07 14:20:59 +0100

    add updates in server config

--- src/server/main.go
+++ src/server/main.go
@@ -7,13 +7,34 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"time"
 
+	"github.com/davecgh/go-spew/spew"
 	bolt "github.com/moutoum/golang-neo4j-bolt-driver"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
+
+func createNeo4jURL(ctx *cli.Context) (string, error) {
+	login := ctx.GlobalString("neo4j-login")
+	if len(login) == 0 {
+		return "", errors.New("missing neo4j login")
+	}
+
+	password := ctx.GlobalString("neo4j-password")
+	if len(password) == 0 {
+		return "", errors.New("missing neo4j password")
+	}
+
+	url := ctx.GlobalString("neo4j-bolt-url")
+	if len(url) == 0 {
+		return "", errors.New("missing neo4j url")
+	}
+
+	return fmt.Sprintf("bolt://%s:%s@%s", login, password, url), nil
+}
 
 func run(ctx *cli.Context) error {
 
@@ -22,11 +43,18 @@ func run(ctx *cli.Context) error {
 		err  error
 	)
 
+	url, err := createNeo4jURL(ctx)
+	if err != nil {
+		return err
+	}
+
 	driv := bolt.NewDriver()
 	ticker := time.Tick(1 * time.Second)
 	limit := time.Now().Add(10 * time.Second)
+
 	for t := <-ticker; t.Before(limit); t = <-ticker {
-		if conn, err = driv.OpenNeo("bolt://neo4j:Ab;q*Ile@neo4j:7687"); err != nil {
+		if conn, err = driv.OpenNeo(url); err != nil {
+			conn = nil
 			logrus.WithError(err).Warn("cannot open neo4j conn")
 		}
 	}
@@ -40,7 +68,11 @@ func run(ctx *cli.Context) error {
 		return fmt.Errorf("cannot query: %v", err)
 	}
 
-	fmt.Println(r)
+	spew.Config.Indent = "\t"
+
+	for data, _, err := r.NextNeo(); err != io.EOF; data, _, err = r.NextNeo() {
+		spew.Dump(data)
+	}
 
 	return nil
 }
@@ -53,9 +85,20 @@ func main() {
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:  "neo4j-bolt-url",
-			Usage: "Neo4j url to connect on",
-			Value: "bolt://neo4j:7687",
+			Name:   "neo4j-bolt-url",
+			Usage:  "Neo4j url to connect on",
+			Value:  "bolt://neo4j:7687",
+			EnvVar: "NEO4J_BOLT_URL",
+		},
+		cli.StringFlag{
+			Name:   "neo4j-login",
+			Usage:  "Account login",
+			EnvVar: "NEO4J_LOGIN",
+		},
+		cli.StringFlag{
+			Name:   "neo4j-password",
+			Usage:  "Account password",
+			EnvVar: "NEO4J_PASSWORD",
 		},
 	}
 
