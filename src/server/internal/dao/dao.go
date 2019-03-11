fee956df18bb4dcdb7411d4da0829ce9409800be
Ubuntu <brendan.allain@epitech.eu>
2019-03-11 03:28:17 -0700

    create job_manager.go

--- src/server/internal/dao/dao.go
+++ src/server/internal/dao/dao.go
@@ -15,12 +15,6 @@ type Company struct {
 	description string `json:"description"`
 }
 
-type Job struct {
-	name        string `json:"name"`
-	description string `json:"description"`
-	grossWage   string `json:"grossWage"`
-}
-
 type DataManager struct {
 	conn bolt.Conn
 }
@@ -59,50 +53,6 @@ func (b *DataManager) GetCompanyWithName(pattern string) error {
 		fmt.Println(results[i].siret)
 		fmt.Println(results[i].siren)
 		fmt.Println(results[i].description)
-	}
-
-	return nil
-}
-
-func (b *DataManager) GetJobWithName(pattern string) error {
-	var results []Job
-
-	r, err := b.conn.QueryNeo("MATCH (j:Job) WHERE j.name CONTAINS {pattern} RETURN j", map[string]interface{}{
-		"pattern": pattern,
-	})
-	if err != nil {
-		return fmt.Errorf("cannot query: %v", err)
-	}
-	defer r.Close()
-
-	for data, _, err := r.NextNeo(); err != io.EOF; data, _, err = r.NextNeo() {
-		d := data[0].(graph.Node)
-
-		p := Job{
-			name:        d.Properties["name"].(string),
-			description: d.Properties["description"].(string),
-			grossWage:   d.Properties["grossWage"].(string),
-		}
-		results = append(results, p)
-	}
-
-	for i := range results {
-		fmt.Println(results[i].name)
-		fmt.Println(results[i].description)
-		fmt.Println(results[i].grossWage)
-	}
-
-	return nil
-}
-
-func (b *DataManager) SetJob(name string, description string, grossWage string) error {
-	_, err := b.conn.ExecNeo("CREATE (j:Job {name: {name}, description: {description}, grossWage: {grossWage}})", map[string]interface{}{
-		"name":        name,
-		"description": description,
-		"grossWage":   grossWage,
-	})
-	if err != nil {
-		return fmt.Errorf("cannot query: %v", err)
 	}
 
 	return nil
