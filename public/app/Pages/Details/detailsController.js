7269ea3f474f68da91685428ff433a33d8b9127c
fabien lacorre <fabien.lacorre@yaakaved.com>
2019-03-18 16:01:02 +0100

    fix

--- public/app/Pages/Details/detailsController.js
+++ public/app/Pages/Details/detailsController.js
@@ -10,46 +10,45 @@ Details.config(['$routeProvider', function ($routeProvider) {
   });
 }])
 
-Details.controller('DetailsController', function ($routeParams, $location,$http,$scope) {
-    console.log($routeParams.type)
-    this.type = $routeParams.type;
-    this.id = $routeParams.id;
+Details.controller('DetailsController', function ($routeParams, $location, $http, $scope) {
+  console.log($routeParams.type)
+  this.type = $routeParams.type;
+  this.id = $routeParams.id;
 
-    console.log(this.id)
-    const promise = []
-    if (this.type === "companie"){
-      promise.push($http.get('/api/company/' + this.id))
-    } else if (this.type === "friend") {
-      promise.push($http.get('/api/profile/' + this.id))
-    } else if (this.type === "job"){
-      promise.push($http.get('/api/job/' + this.id))
-    } else if (this.type === "hobbie") {
-      promise.push($http.get('/api/hobby/' + this.id))
-    } else if (this.type === "skill") {
-      promise.push($http.get('/api/skill/' + this.id))
-    }
+  console.log(this.id)
+  const promise = []
+  if (this.type === "companie") {
+    promise.push($http.get('/api/company/' + this.id))
+  } else if (this.type === "friend") {
+    promise.push($http.get('/api/profile/' + this.id))
+  } else if (this.type === "job") {
+    promise.push($http.get('/api/job/' + this.id))
+  } else if (this.type === "hobbie") {
+    promise.push($http.get('/api/hobby/' + this.id))
+  } else if (this.type === "skill") {
+    promise.push($http.get('/api/skill/' + this.id))
+  }
 
-    Promise.all(promise)
+  Promise.all(promise)
     .then((response) => {
       this.object = response[0].data
-      this.object.photo = "../../img/test.jpg",
-
+      this.object.photo = "../../img/test.jpg";
       if (!$scope.$$phase) {
         $scope.$apply();
       }
     })
-    .catch(() => alert('Cannot load informations'))
+  .catch(() => alert('Cannot load informations'))
 
-    // this.object = {
-    //     name: "TEST NAME",
-    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    //     date: new Date(),
-    //     type: this.type,
-    //     photo: "../../img/test.jpg",
-    // }
+// this.object = {
+//     name: "TEST NAME",
+//     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//     date: new Date(),
+//     type: this.type,
+//     photo: "../../img/test.jpg",
+// }
 
-    this.changeLocation = (route) => {
-        $location.path('/' + route)
-    }
+this.changeLocation = (route) => {
+  $location.path('/' + route)
+}
 
 })
